14a9e218893a7cdef228e3269aa2c1b459a7249d
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-19 22:50:36 -0400

    timer working

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 console.log("Test your luck!");
 
+var counter = 300;
+var intervalId;
+
+//on "play" button click begin game by calling the start function in the game variable
+$("#start").on("click", function () {
+    game.start();
+})
+
+var game = {
+  correct: 0,
+  wrong: 0,
+  counter: 10,  
+
+  decrement: function () {
+      game.counter --;
+      $("#counter").html("You have " + game.counter + " seconds left!");
+      if(game.counter <= 0){
+          console.log("time is up!");
+          game.over();
+      }
+  },
+
+  start: function () {
+    console.log("the game began");
+    timer = setInterval(game.decrement,1000);
+  },
+  
+  over: function () {
+      console.log("game over!");
+  }
+}
+
 
